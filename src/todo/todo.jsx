3fb773c1521f292eb8aad1575fb2d1696470a6da
Augusto Scher <augustoscher@gmail.com>
2019-06-13 15:07:17 -0300

    65 - event on change

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -7,18 +7,26 @@ export default class Todo extends React.Component {
 
   constructor(props){
     super(props)
+    this.state = {description: '', list:[]}
     this.handleAdd = this.handleAdd.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleAdd(){
     console.log('handleAdd')
   }
 
+  handleChange(e){
+    this.setState({...this.setState, description: e.target.value})
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"/>
-        <TodoForm handleAdd={this.handleAdd}  />
+        <TodoForm description={this.state.description} 
+            handleAdd={this.handleAdd}  
+            handleChange={this.handleChange}/>
         <TodoList />
       </div>
     )
